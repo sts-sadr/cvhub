89493cf29477c5276d2610d37efe51d196b8c704
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-27 21:51:11 +0330

    Treating image as a numpy array

--- all/chapter_02/number_01.py
+++ all/chapter_02/number_01.py
@@ -7,3 +7,10 @@ import cv2
 image_path = "images/EverLookNeverSee.jpg"
 # read or load image from its path
 image = cv2.imread(image_path)
+
+# image is a Numpy array
+print(f"Dimensions of the image: {image.ndim}")
+print(f"Image height: {image.shape[0]}")
+print(f"Image width: {image.shape[1]}")
+print(f"Image channels: {image.shape[2]}")
+print(f"Size of the image array: {image.size}")
