05b438561b1a3c7d25355866eca9336c12080667
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 19:06:06 +0330

    setting start and end coordinates

--- all/chapter_02/number_03.py
+++ all/chapter_02/number_03.py
@@ -7,3 +7,7 @@ import cv2
 image_path = "images/EverLookNeverSee.jpg"
 # read or load image from its path
 image = cv2.imread(image_path)
+
+# setting start and end coordinates
+start = (0, 0)
+end = (image.shape[1], image.shape[0])
