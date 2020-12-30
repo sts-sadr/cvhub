60177bfbd8e8eeb9f52b0cbd47e2ebef1d006ea1
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 19:34:11 +0330

    loading image

---
+++ all/chapter_02/number_04.py
@@ -0,0 +1,9 @@
+# Drawing a rectangle on an image
+
+# importing relevant libraries
+import cv2
+
+# image path
+image_path = "images/EverLookNeverSee.jpg"
+# read or load image from its path
+image = cv2.imread(image_path)
