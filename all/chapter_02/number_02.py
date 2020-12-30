37c674a037f755a52216263f13b485e1598b4dfa
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 18:50:31 +0330

    Accessing pixel at (0, 0) location

--- all/chapter_02/number_02.py
+++ all/chapter_02/number_02.py
@@ -7,3 +7,7 @@ import cv2
 image_path = "images/EverLookNeverSee.jpg"
 # read or load image from its path
 image = cv2.imread(image_path)
+
+# Accessing pixel at (0, 0) location
+(b, g, r) = image[0, 0]
+print(f"Blue, green and red values at (0, 0); {(b, g, r)}")
