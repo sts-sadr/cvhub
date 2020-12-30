55f81c61edf8e0d848b73f8e7fbf20877750e1fc
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 21:26:23 +0330

    setting circle features

--- all/chapter_02/Number_06.py
+++ all/chapter_02/Number_06.py
@@ -6,3 +6,9 @@ import numpy as np
 
 # creating a new canvas
 canvas = np.zeros((500, 500, 3), dtype="uint8")
+
+# setting circle features
+center = (250, 250)
+radius = 100
+color = (0, 0, 255)
+thickness = 1
