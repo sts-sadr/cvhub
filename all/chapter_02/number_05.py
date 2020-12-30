62f40b17c33c249697e3cd9266e3f4861f8d0854
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 20:58:52 +0330

    creating new canvas, setting start, end and thickness

--- all/chapter_02/number_05.py
+++ all/chapter_02/number_05.py
@@ -3,3 +3,12 @@
 # importing relevant libraries
 import cv2
 import numpy as np
+
+# creating a new canvas
+canvas = np.zeros((200, 200, 3), dtype="uint8")
+# setting start and end coordinates of the top-left
+# and bottom-right  of rectangle
+start = (10, 10)
+end = (100, 100)
+# setting the thickness
+thickness = 5
