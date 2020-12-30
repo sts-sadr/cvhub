b92405fd49ab84fa1a4f94ed1da6a9b5ff1ca08e
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-30 19:38:41 +0330

    setting the color and thickness

--- all/chapter_02/number_04.py
+++ all/chapter_02/number_04.py
@@ -8,8 +8,11 @@ image_path = "images/EverLookNeverSee.jpg"
 # read or load image from its path
 image = cv2.imread(image_path)
 
-
 # setting the start and end coordinates of top-left
 # and bottom-right of the rectangle
 start = (30, 30)
 end = (370, 370)
+
+# setting the color and thickness of the outline
+color = (0, 255, 0)
+thickness = 2
