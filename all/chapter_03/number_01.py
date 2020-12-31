8f0e542f3a1bff365bd516b86367138c7f7b0eba
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-12-31 21:51:13 +0330

    calculating aspect ratio

--- all/chapter_03/number_01.py
+++ all/chapter_03/number_01.py
@@ -7,3 +7,8 @@ import numpy as np
 # loading image
 image_path = "images/918_spyder.jpg"
 image = cv2.imread(image_path)
+
+# getting image shape which returns height, width and channels
+# as a tuple and then calculating the aspect ratio
+(h, w) = image.shape[:2]
+aspect_ratio = w / h
