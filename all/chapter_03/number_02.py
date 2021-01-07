438b990ab114a1c6dfddfb293ed0ccba5adc1e70
ELNS <EverLookNeverSee@ProtonMail.ch>
2021-01-07 21:30:51 +0330

    defining translation matrix

--- all/chapter_03/number_02.py
+++ all/chapter_03/number_02.py
@@ -8,3 +8,9 @@ import numpy as np
 # Loading image
 image_path = "./images/918_spyder.jpg"
 image = cv2.imread(image_path)
+
+# Defining translation matrix
+translation_matrix = np.float32([
+    [1, 0, 50],
+    [0, 1, 20]
+])
