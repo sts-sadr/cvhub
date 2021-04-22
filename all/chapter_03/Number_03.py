2ce3854d45db23e8b9b3e0ca6952460780bbe059
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-22 09:57:24 +0430

    Defining the rotation matrix

--- all/chapter_03/Number_03.py
+++ all/chapter_03/Number_03.py
@@ -6,3 +6,9 @@ import cv2
 image_path = "images/918_spyder.jpg"
 image = cv2.imread(image_path)
 (h, w) = image.shape[:2]
+
+# Defining the rotation matrix
+center = (h // 2, w // 2)
+angle = -45
+scale = 1.0
+rotation_matrix = cv2.getRotationMatrix2D(center, angle, scale)
