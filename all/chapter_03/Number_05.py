83fa33962c99d67126959f68d6d3f6aba54106ff
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-22 11:58:25 +0430

    Cropping image

--- all/chapter_03/Number_05.py
+++ all/chapter_03/Number_05.py
@@ -7,4 +7,8 @@ import cv2
 image_path = "images/918_spyder.jpg"
 image = cv2.imread(image_path)
 cv2.imshow("Original Image", image)
+
+# Cropping process
+cropped_image = image[300:800, 500:1000]
+cv2.imshow("Cropped Image", cropped_image)
 cv2.waitKey(0)
