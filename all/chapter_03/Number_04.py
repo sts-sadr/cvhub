91cd1448272799754ae44c5c693e3b60f9995b0f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-22 11:27:55 +0430

    Implementation of vertical flip

--- all/chapter_03/Number_04.py
+++ all/chapter_03/Number_04.py
@@ -11,3 +11,8 @@ image = cv2.imread(image_path)
 flip_horizontally = cv2.flip(image, 1)
 cv2.imshow("Flipped Horizontally", flip_horizontally)
 cv2.waitKey(-1)
+
+# Flip vertically
+flip_vertically = cv2.flip(image, 0)
+cv2.imshow("Flipped Vertically", flip_vertically)
+cv2.waitKey(-1)
