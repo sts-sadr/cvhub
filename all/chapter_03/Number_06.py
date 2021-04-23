23faa126633811aea700fc85fc38aca0c18e2518
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-23 15:38:31 +0430

    Loading two images

--- all/chapter_03/Number_06.py
+++ all/chapter_03/Number_06.py
@@ -2,3 +2,11 @@
 
 # import statements
 import cv2
+
+
+# Loading images
+image_1_path = "images/Abraham_Lincoln.jpg"
+image_2_path = "images/Albert_Einstein.jpg"
+
+image_1 = cv2.imread(image_1_path)
+image_2 = cv2.imread(image_2_path)
