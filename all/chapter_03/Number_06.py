71cb6c7d0c4d7b0c6cf06caa169bf4c9fbc9ecc5
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-23 15:44:14 +0430

    Adding two images and displaying the differences

--- all/chapter_03/Number_06.py
+++ all/chapter_03/Number_06.py
@@ -15,3 +15,13 @@ image_2 = cv2.imread(image_2_path)
 # Resizing the images to make them of the same dimension
 resized_images_1 = cv2.resize(image_1, (800, 800), interpolation=cv2.INTER_AREA)
 resized_images_2 = cv2.resize(image_2, (800, 800), interpolation=cv2.INTER_AREA)
+
+
+# Addition process
+result = cv2.add(resized_images_1, resized_images_2)
+
+# # Displaying the images in order to see differences
+cv2.imshow("Resized image 1", resized_images_1)
+cv2.imshow("Resized image 2", resized_images_2)
+cv2.imshow("Addition result", result)
+cv2.waitKey(0)
