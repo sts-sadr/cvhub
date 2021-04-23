637e836170c42a5642b8bf3edfb9d6304d17a403
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-23 15:52:46 +0430

    Adding enhancement process

--- all/chapter_03/Number_06.py
+++ all/chapter_03/Number_06.py
@@ -24,6 +24,9 @@ simple_addition_result = cv2.add(resized_images_1, resized_images_2)
 weighted_addition_process = cv2.addWeighted(resized_images_1, 0.7,
                                             resized_images_2, 0.3, 0)
 
+# Enhancement process
+enhanced_image = 255 * resized_images_1
+
 
 # # Displaying the images in order to see differences
 cv2.imshow("Resized image 1", resized_images_1)
