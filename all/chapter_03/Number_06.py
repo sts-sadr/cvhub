ec6316a7eb3dd3e0b370972473d25239ee1ac387
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-23 15:40:46 +0430

    Resizing the images

--- all/chapter_03/Number_06.py
+++ all/chapter_03/Number_06.py
@@ -10,3 +10,8 @@ image_2_path = "images/Albert_Einstein.jpg"
 
 image_1 = cv2.imread(image_1_path)
 image_2 = cv2.imread(image_2_path)
+
+
+# Resizing the images to make them of the same dimension
+resized_images_1 = cv2.resize(image_1, (800, 800), interpolation=cv2.INTER_AREA)
+resized_images_2 = cv2.resize(image_2, (800, 800), interpolation=cv2.INTER_AREA)
