d03f20184e8f5876360e2f1bd36f0b6acbe5cdcd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-28 23:26:30 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_07.py
@@ -0,0 +1,20 @@
+"""
+    Bitwise Operations
+
+    AND, OR, NOT, XOR
+    For grayscale binary images the pixel value 0 means off and value greater than 0 means on.
+
+    AND:
+        The bitwise AND operation of two image arrays calculates element-wise conjunction.
+        Bitwise AND can also be performed with an array and a scalar.
+
+    OR:
+        The bitwise OR operation calculates element-wise disjunction of two arrays or an array and a scalar.
+
+    NOT:
+        Bitwise NOT inverts the bit values of its operand.
+
+    XOR:
+        A bitwise XOR of the two operands “a” and “b” results in 1 if either but not both “a” or “b”
+        is 1; otherwise, the result is 0.
+"""
