4f8ea3a5ecc6fd015d9af6d30b405d24dedaae60
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-29 16:50:16 +0430

    Creating the mask

--- all/chapter_03/Number_08.py
+++ all/chapter_03/Number_08.py
@@ -13,3 +13,9 @@ import numpy as np
 # Loading sample image
 sample_image = cv2.imread("images/Albert_Einstein.jpg")
 cv2.imshow("Albert Einstein", sample_image)
+
+# Creating a rectangular mask
+mask_image = cv2.rectangle(np.zeros(sample_image.shape[:2], dtype="uint8"), (50, 50),
+                           (int(sample_image.shape[1]) - 50, int(sample_image.shape[0] / 2) - 50),
+                           (255, 255, 255), -1)
+cv2.imshow("Mask Image", mask_image)
