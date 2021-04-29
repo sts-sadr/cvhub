98d4cf203cf67fc628b161f3f815558dbee84ded
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-29 19:33:41 +0430

    Splitting

--- all/chapter_03/Number_09.py
+++ all/chapter_03/Number_09.py
@@ -11,3 +11,6 @@ import numpy as np
 # Loading an image
 sample_image = cv2.imread("images/Abraham_Lincoln.jpg")
 cv2.imshow("Abraham Lincoln", sample_image)
+
+# Splitting the image into component colors
+(b, g, r) = cv2.split(sample_image)
