dc90df3164ed9b02cf370532a6b984e9652616d7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-29 16:40:50 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_08.py
@@ -0,0 +1,7 @@
+"""
+    Masking:
+        Masking refers to the “hiding” or “filtering” of an image.
+        When we mask an image, we hide a portion of the image with some other image.
+        In other words, we put our focus on a portion of the image by applying a mask
+        on the remaining portion of the image.
+"""
