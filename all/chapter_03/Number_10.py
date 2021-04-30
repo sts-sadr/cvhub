9fa46540883a9912c247989802f4fca338ed5018
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-04-30 14:31:13 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_10.py
@@ -0,0 +1,11 @@
+"""
+    Noise reduction using smoothing and blurring
+
+    Smoothing, also called blurring, is an important image processing technique to reduce
+    noise present in an image.
+
+    Types of noise that we encounter in an image:
+        - Salt and pepper noise
+        - Impulse noise
+        - Gaussian noise
+"""
