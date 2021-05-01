3879108ae45fad11b46acda21a4ea94a730239f9
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-01 22:46:09 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_12.py
@@ -0,0 +1,8 @@
+"""
+    Noise reduction using smoothing and blurring
+
+    Median Blurring:
+        Median blurring is an effective technique for reducing salt-and-pepper type of noise.
+        Median blurring is similar to mean blurring except that the central value of the kernel
+        is replaced by the median of the surrounding pixels.
+"""
