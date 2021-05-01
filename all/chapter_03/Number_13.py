567a948c1678ab8747178c903f9949d86e06446c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-01 23:06:50 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_13.py
@@ -0,0 +1,13 @@
+"""
+    Noise reduction using smoothing and blurring
+
+    Bilateral blurring:
+        The previous three blurring techniques yield blurred images with the side effect that we
+        lose the edges in the image. To blur an image while preserving the edges, we use bilateral
+        blurring, which is an enhancement over Gaussian blurring. Bilateral blurring takes two
+        Gaussian distributions to perform the computation. The first Gaussian function considers
+        the spatial neighbors (pixels in x and y space that are close together). The second Gaussian
+        function considers the pixel intensity of the neighboring pixels. This makes sure that only
+        those pixels that are of similar intensity to the central pixel are considered for blurring,
+        leaving the edges intact as the edges tend to have higher intensity compared to other pixels.
+"""
