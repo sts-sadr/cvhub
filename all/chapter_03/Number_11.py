f5958abdc8873ab1f0ddc4e9e9bd110c11f1772a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-01 21:37:27 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_11.py
@@ -0,0 +1,8 @@
+"""
+    Noise reduction using smoothing and blurring
+
+    Gaussian filtering:
+        This blurring technique gives a more natural smoothing result compared to the averaging technique.
+        In this filtering, we supply a Gaussian kernel instead of a boxed fixed kernel.
+        A Gaussian kernel consists of the height, width, and standard deviations in the X and Y directions.
+"""
