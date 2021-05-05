01d803387eef69667562b9b8ac53591371b17c9e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-05 22:38:49 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_14.py
@@ -0,0 +1,10 @@
+"""
+    Binarization with Thresholding
+
+    Image binarization is the process of converting a grayscale image into a binary—a black
+    and-white—image. We apply a technique called thresholding to binarize an image. We first
+    decide on a threshold value. A pixel value greater than this threshold is changed to 255,
+    and a pixel with a lesser value than the threshold is set to 0. The resultant image will
+    have only two values of the pixels—0 and 255—which are black-and-white color values. Thus,
+    a grayscale image is converted into a black-and-white image (also called a binary image).
+"""
