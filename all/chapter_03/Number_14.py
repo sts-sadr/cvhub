0bad153f160cd38731ea2d93d346f24bced22341
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-05 22:54:47 +0430

    Converting to grayscale

--- all/chapter_03/Number_14.py
+++ all/chapter_03/Number_14.py
@@ -11,9 +11,14 @@
 
 # Simple Thresholding
 
-from cv2 import imread, imshow, waitKey, threshold, THRESH_BINARY, THRESH_BINARY_INV
+from cv2 import (imread, imshow, waitKey, threshold, THRESH_BINARY, THRESH_BINARY_INV,
+                 cvtColor, COLOR_BGR2GRAY)
 
 
 # Loading the image
 original_image = imread("images/Abraham_Lincoln.jpg")
 imshow("Original Image", original_image)
+
+# Converting to grayscale
+grayscale_image = cvtColor(original_image, COLOR_BGR2GRAY)
+imshow("GrayScale Image", grayscale_image)
