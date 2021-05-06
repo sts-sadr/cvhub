a30657ba9507e9829e42b19f11070c7dd099940d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-06 23:17:35 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_16.py
@@ -0,0 +1,10 @@
+"""
+    Otsu's Binarization
+
+    In the simple thresholding, we select a global threshold that is arbitrarily selected. It is
+    difficult to know what the right value of the threshold is, so we may need to do trial-and-
+    error experiments a few times before you get the right value. Even if you get an ideal value
+    for one case, it may not work with other images that have different pixel intensity characteristics.
+    Otsu’s method determines an optimal global threshold value from the image histogram. For now, just
+    think of the histogram as the frequency distribution of pixel values.
+"""
