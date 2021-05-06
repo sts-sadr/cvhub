46e3cbde93382ed949434b0481ec9b1882cc9d92
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-06 22:43:57 +0430

    Converting to grayscale

--- all/chapter_03/Number_15.py
+++ all/chapter_03/Number_15.py
@@ -17,3 +17,7 @@ from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, ADAPTIVE_THR
 # Loading the image
 original_image = imread("images/Abraham_Lincoln.jpg")
 imshow("Original Image", original_image)
+
+# Converting to grayscale
+grayscale_image = cvtColor(original_image, COLOR_BGR2GRAY)
+imshow("GrayScale Image", grayscale_image)
