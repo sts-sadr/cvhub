c3331ba369e040ca2e897e939e9572bc36552853
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-06 22:36:18 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_15.py
@@ -0,0 +1,9 @@
+"""
+    Adaptive Thresholding
+
+    Adaptive thresholding is used to binarize a grayscale image that has a varying degree
+    of pixel intensity, and one single threshold value may not be suitable to extract the
+    information from the image. In adaptive thresholding, the algorithm determines the
+    threshold for a pixel based on a small region around it. This will get us a different
+    threshold value for different regions in the same image.
+"""
