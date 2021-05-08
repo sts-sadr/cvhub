c5ce084d735a14fb151dfad73b6760d1e7cb8687
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-08 23:24:59 +0430

    Adding title comment and importing relevant libraries

--- all/chapter_03/Number_17.py
+++ all/chapter_03/Number_17.py
@@ -17,3 +17,9 @@
         an argument ksize that we use to define the kernel size. If we set ksize to -1, OpenCV will internally
         apply a 3×3 Schar filter, which generally gives a better result compared to the 3×3 Sobel filter.
 """
+
+# Sobel derivatives
+
+from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, bilateralFilter,
+           Sobel, CV_64F)
+from numpy import (uint8, absolute)
