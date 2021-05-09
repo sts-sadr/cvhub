78a4e7999c46882df0d0bbb4662d7a6a9f491219
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-09 10:10:37 +0430

    Adding initial docstring

---
+++ all/chapter_03/Number_18.py
@@ -0,0 +1,10 @@
+"""
+    Gradients and Edge Detection
+
+    Laplacian Derivatives:
+        The Laplacian operator calculates the second derivative of the pixel intensity function to
+        determine the edges in the image. The Laplacian operator calculates the gradients based on
+        the following equation:
+
+            Laplacian(f) = \frac{\partial^2f}{\partial x^2} + \frac{\partial^2f}{\partial y^2}
+"""
