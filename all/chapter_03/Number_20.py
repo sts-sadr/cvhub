06db0656a3d9222a0e7959b433d22f9d87887983
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-09 13:11:51 +0430

    Adding title comment and importing relevant libraries

--- all/chapter_03/Number_20.py
+++ all/chapter_03/Number_20.py
@@ -9,3 +9,8 @@
         4. Find all the contours using findContours method
         5. Draw contours using drawContours method
 """
+
+# Detecting and drawing contours
+
+from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, threshold, THRESH_BINARY_INV,
+                 THRESH_OTSU, Canny, findContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, drawContours)
