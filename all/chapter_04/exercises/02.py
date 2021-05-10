91c8cdd561937d5c60a56ed25efcf0a12e8df94e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 20:49:17 +0430

    Defining colors sequence and calculating histogram

--- all/chapter_04/exercises/02.py
+++ all/chapter_04/exercises/02.py
@@ -20,3 +20,11 @@ imshow("Mask Image", mask_image)
 # Performing mask using bitwise_and operator
 masked_image = bitwise_and(original_image, original_image, mask=mask_image)
 imshow("Masked Image", masked_image)
+
+# Defining colors sequence
+colors = ("blue", "green", "red")
+# Calculating the histogram
+for i, color in enumerate(colors):
+    hist = calcHist([masked_image], [i], None, [256], [0, 255])
+    # plotting the histogram graph
+    plt.plot(hist, color=color)
