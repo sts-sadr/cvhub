04f48a5ecc558b4bc8f85d48794c8f27edff85cc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 19:45:37 +0430

    plotting the histogram

--- all/chapter_04/Number_02.py
+++ all/chapter_04/Number_02.py
@@ -9,9 +9,16 @@ original_image = imread("images/Bill-Gates.jpg")
 imshow("Original Color Image", original_image)
 
 # OpenCV stores color in BGR sequence
-colors = ("blue", "green", "ged")
+colors = ("blue", "green", "red")
 # Calculating the histogram
 for i, color in enumerate(colors):
     hist = calcHist([original_image], [i], None, [256], [0, 255])
     # plotting the histogram graph
     plt.plot(hist, color=color)
+
+# plotting the histogram
+plt.title("RGB Color Histogram")
+plt.xlabel("Bins")
+plt.ylabel("Number of pixels")
+plt.show()
+waitKey(0)
