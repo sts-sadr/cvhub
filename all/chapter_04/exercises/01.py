39650abe7a9dffab9d91fe8b02b9942c0b1bc350
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 19:56:27 +0430

    Adding initial description and importing relevant modules

---
+++ all/chapter_04/exercises/01.py
@@ -0,0 +1,7 @@
+"""
+    Exercise 01:
+        Plot a histogram of an image with 32 bins
+"""
+
+from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, calcHist)
+import matplotlib.pyplot as plt
