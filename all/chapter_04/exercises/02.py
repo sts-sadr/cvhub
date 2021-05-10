a1bdd98c73199f55c34465fd673ade08b89425c3
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 20:45:07 +0430

    Performing mask

--- all/chapter_04/exercises/02.py
+++ all/chapter_04/exercises/02.py
@@ -3,7 +3,7 @@
         Create a histogram of a masked image
 """
 
-from cv2 import (imread, imshow, waitKey, calcHist, rectangle)
+from cv2 import (imread, imshow, waitKey, calcHist, rectangle, bitwise_and)
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,3 +16,7 @@ mask_image = rectangle(np.zeros(original_image.shape[:2], dtype="uint8"), (50, 5
                            (int(original_image.shape[1]) - 50, int(original_image.shape[0] / 2) - 50),
                            (255, 255, 255), -1)
 imshow("Mask Image", mask_image)
+
+# Performing mask using bitwise_and operator
+masked_image = bitwise_and(original_image, original_image, mask=mask_image)
+imshow("Masked Image", masked_image)
