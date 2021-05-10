cea50972e58c3a0ec5bedba2fafc0f8b94123a6f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 19:33:09 +0430

    Loading the image

--- all/chapter_04/Number_02.py
+++ all/chapter_04/Number_02.py
@@ -2,3 +2,8 @@
 
 from cv2 import (imread, imshow, waitKey, calcHist)
 import matplotlib.pyplot as plt
+
+
+# Loading the image
+original_image = imread("images/Bill-Gates.jpg")
+imshow("Original Color Image", original_image)
