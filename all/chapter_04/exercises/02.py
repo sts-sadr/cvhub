652c0e96ecb0ab18f2fe0859c761139cc0552f01
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 20:42:53 +0430

    Creating mask image

--- all/chapter_04/exercises/02.py
+++ all/chapter_04/exercises/02.py
@@ -3,10 +3,16 @@
         Create a histogram of a masked image
 """
 
-from cv2 import (imread, imshow, waitKey, calcHist)
+from cv2 import (imread, imshow, waitKey, calcHist, rectangle)
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Loading the image
 original_image = imread("../images/Bill-Gates.jpg")
 imshow("Original Image", original_image)
+
+# Creating the rectangular mask
+mask_image = rectangle(np.zeros(original_image.shape[:2], dtype="uint8"), (50, 50),
+                           (int(original_image.shape[1]) - 50, int(original_image.shape[0] / 2) - 50),
+                           (255, 255, 255), -1)
+imshow("Mask Image", mask_image)
