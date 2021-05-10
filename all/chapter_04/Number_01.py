dae7eeaa99116faf248507008af6784ced591dc1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 11:51:05 +0430

    Adding initial docstring

---
+++ all/chapter_04/Number_01.py
@@ -0,0 +1,9 @@
+"""
+    Color Histogram
+
+        A histogram is the distribution of pixel intensities in an image. Typically a histogram is
+        visualized in the form of a graph (or chart). The x-axis of this graph represents the pixel
+        values (or a range of values), and the y-axis represents the frequency (or count) of pixels
+        of a particular value or a range of values. The peak of the graph shows the color with the
+        highest number of pixels.
+"""
