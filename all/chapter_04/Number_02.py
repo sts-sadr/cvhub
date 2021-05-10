2d6a01eb3ef74a0d15c38ba232cefd3af1d8b4d6
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-10 19:42:19 +0430

    Defining color sequences and calculating histogram

--- all/chapter_04/Number_02.py
+++ all/chapter_04/Number_02.py
@@ -7,3 +7,11 @@ import matplotlib.pyplot as plt
 # Loading the image
 original_image = imread("images/Bill-Gates.jpg")
 imshow("Original Color Image", original_image)
+
+# OpenCV stores color in BGR sequence
+colors = ("blue", "green", "ged")
+# Calculating the histogram
+for i, color in enumerate(colors):
+    hist = calcHist([original_image], [i], None, [256], [0, 255])
+    # plotting the histogram graph
+    plt.plot(hist, color=color)
