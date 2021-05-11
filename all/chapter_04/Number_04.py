626e5761456f5d19bbfa6df7928399bb971f04c4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 12:26:46 +0430

    Adding initial docstring

---
+++ all/chapter_04/Number_04.py
@@ -0,0 +1,7 @@
+"""
+    GLCM:
+        The gray-level co-occurrence matrix (GLCM) is the distribution of simultaneously
+        occurring pixel values within a given offset. An offset is the position (distance
+        and direction) of adjacent pixels. As the name implies, the GLCM is always calculated
+        for a grayscale image.
+"""
