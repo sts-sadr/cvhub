159b598f800c1c26e5c5f2f37f4eb85a32b68d5e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 12:59:46 +0430

    Calculating glcm and contrast

--- all/chapter_04/Number_05.py
+++ all/chapter_04/Number_05.py
@@ -1,7 +1,7 @@
 # Calculation of Image Statistics from the GLCM
 
 from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY)
-from skimage.feature import greycomatrix
+from skimage.feature import greycomatrix, greycoprops
 from numpy import pi
 
 
@@ -9,3 +9,10 @@ from numpy import pi
 original_image = imread("images/Bill-Gates.jpg")
 grayscale_image = cvtColor(original_image, COLOR_BGR2GRAY)
 imshow("Grayscale Image", grayscale_image)
+
+# Calculating GLCM
+glcm = greycomatrix(grayscale_image, [2], [0, pi / 2])
+
+# Calculating the contrast
+contrast = greycoprops(glcm)
+print(f"Contrast: {contrast}")
