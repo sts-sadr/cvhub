454d3462bcb531890ea6c1145920274355a0d21a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 12:32:39 +0430

    Importing relevant libraries

--- all/chapter_04/Number_04.py
+++ all/chapter_04/Number_04.py
@@ -5,3 +5,6 @@
         and direction) of adjacent pixels. As the name implies, the GLCM is always calculated
         for a grayscale image.
 """
+
+from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY)
+import skimage.feature as sk
