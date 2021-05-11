efcd11fd5a70a9becb1e8d9784e9b1d2ee77e790
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 10:39:20 +0430

    Adding initial docstring

---
+++ all/chapter_04/Number_03.py
@@ -0,0 +1,6 @@
+"""
+    Histogram Equalizer:
+        Histogram equalization is an image processing technique to adjust the contrast of an image.
+        It is a method of redistributing the pixel intensities in such a way that the intensities of
+        the under-populated pixels are equalized to the intensities of over-populated pixel intensities
+"""
