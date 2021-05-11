ea024d48fe7cfc6f621b4d871da90b452819ed2c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 12:46:08 +0430

    Reading the image and converting it to grayscale

--- all/chapter_04/Number_04.py
+++ all/chapter_04/Number_04.py
@@ -8,3 +8,9 @@
 
 from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY)
 import skimage.feature as sk
+
+
+# Reading the image and converting it to grayscale
+original_image = imread("images/Bill-Gates.jpg")
+grayscale_image = cvtColor(original_image, COLOR_BGR2GRAY)
+imshow("Grayscale Image", grayscale_image)
