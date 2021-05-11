87f4274af2e8c0fff159b6a2b816e7d08345a123
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 10:41:50 +0430

    Importing relevant modules

--- all/chapter_04/Number_03.py
+++ all/chapter_04/Number_03.py
@@ -4,3 +4,6 @@
         It is a method of redistributing the pixel intensities in such a way that the intensities of
         the under-populated pixels are equalized to the intensities of over-populated pixel intensities
 """
+
+from cv2 import (imread, imshow, waitKey, cvtColor, COLOR_BGR2GRAY, calcHist, equalizeHist)
+import matplotlib.pyplot as plt
