a2750a8aa68615031675c3411824959d3aa07c8f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-11 10:51:57 +0430

    Calculating and plotting histogram without using equalizer

--- all/chapter_04/Number_03.py
+++ all/chapter_04/Number_03.py
@@ -13,3 +13,14 @@ import matplotlib.pyplot as plt
 original_image = imread("images/Bill-Gates.jpg")
 grayscale_image = cvtColor(original_image, COLOR_BGR2GRAY)
 imshow("GrayScale Image", grayscale_image)
+
+# Calculating the histogram without using equalizer
+hist = calcHist([grayscale_image], [0], None, [256], [0, 255])
+
+# Plotting the histogram graph
+plt.figure()
+plt.title("Grayscale Histogram Without Using Equalizer")
+plt.xlabel("Bins")
+plt.ylabel("Number of pixels")
+plt.plot(hist)
+plt.show()
