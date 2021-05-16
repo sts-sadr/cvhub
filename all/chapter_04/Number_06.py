6ccd9a97ff05249306f212374280456c9253daa7
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-16 20:20:16 +0430

    Adding initial docstring

---
+++ all/chapter_04/Number_06.py
@@ -0,0 +1,7 @@
+"""
+    HOGs:
+        Histograms of oriented gradients (HOGs) are important feature descriptors used
+        in computer vision and machine learning for object detection. HOGs describe the
+        structural shape and appearance of an object in an image. The HOG algorithm computes
+        the occurrences of gradient orientation in localized portions of the image.
+"""
