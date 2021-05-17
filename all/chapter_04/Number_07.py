80375fc3b085d3864478402852c0d23003a2f40f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-17 23:41:53 +0430

    Adding title comment and importing relevant modules

--- all/chapter_04/Number_07.py
+++ all/chapter_04/Number_07.py
@@ -2,3 +2,9 @@
     LBP:
         Local binary patterns (LBP) is a type of feature descriptor for image texture classification.
 """
+
+# LBP Image and Histogram Calculation
+
+from numpy import histogram
+from skimage.feature import local_binary_pattern
+from cv2 import (imread, imshow, waitKey, resize, cvtColor, COLOR_BGR2GRAY, calcHist)
