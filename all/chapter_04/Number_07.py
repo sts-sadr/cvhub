a8a8c673da0ac19f95984015cd30044b96ba2dc0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-17 23:52:04 +0430

    Plotting the histogram of grayscale image

--- all/chapter_04/Number_07.py
+++ all/chapter_04/Number_07.py
@@ -6,6 +6,7 @@
 # LBP Image and Histogram Calculation
 
 from numpy import histogram
+import matplotlib.pyplot as plt
 from skimage.feature import local_binary_pattern
 from cv2 import (imread, imshow, waitKey, resize, cvtColor, COLOR_BGR2GRAY, calcHist)
 
@@ -18,4 +19,9 @@ grayscale_image = cvtColor(resized_image, COLOR_BGR2GRAY)
 imshow("GrayScale Image", grayscale_image)
 
 # Calculating histogram of grayscale image
-original_hist = calcHist(grayscale_image, [0], None, [256], [0, 255])
+grayscale_hist = calcHist(grayscale_image, [0], None, [256], [0, 255])
+
+# Plotting the histogram of grayscale image
+plt.figure()
+plt.title("Histogram of grayscale image")
+plt.plot(grayscale_hist, color="r")
