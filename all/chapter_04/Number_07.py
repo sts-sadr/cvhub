9e214bca02ef8c433274d5999a9f2af6230ac4c8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-17 23:46:56 +0430

    Load, resize and convert to grayscale

--- all/chapter_04/Number_07.py
+++ all/chapter_04/Number_07.py
@@ -8,3 +8,11 @@
 from numpy import histogram
 from skimage.feature import local_binary_pattern
 from cv2 import (imread, imshow, waitKey, resize, cvtColor, COLOR_BGR2GRAY, calcHist)
+
+
+# Loading the image, resizing and converting to grayscale
+original_image = imread("images/Bill-Gates.jpg")
+resized_image = resize(original_image, (int(original_image.shape[0] / 5),
+                                        int(original_image.shape[1] / 5)))
+grayscale_image = cvtColor(resized_image, COLOR_BGR2GRAY)
+imshow("GrayScale Image", grayscale_image)
