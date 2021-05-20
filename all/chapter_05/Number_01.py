8b644c00daf72a73fe74411e212c3f2a839b494b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-05-20 23:56:28 +0430

    Adding initial docstring

---
+++ all/chapter_05/Number_01.py
@@ -0,0 +1,12 @@
+"""
+    Activation functions of artificial neural networks implemented from scratch
+
+        - Linear
+        - Sigmoid or Logistic
+        - Hyperbolic Tangent
+        - Rectified Linear Unit (ReLu)
+        - Leaky ReLu
+        - Scaled Exponential Linear Unit (SELU)
+        - Softplus
+        - Softmax
+"""
